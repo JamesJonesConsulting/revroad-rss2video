f108aa16a4518adf549c8914089aa5a49058c7c7
James Jones <jamjon3@gmail.com>
2016-12-15 22:18:41 -0500

    Lots of updates

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ var http = require('http');
 var dateFormat = require('dateformat');
 var fs = require('fs');
 var CommandQueue = require("command-queue");
+
 if (process.argv.length <= 2) {
     console.log("Usage: " + __filename + " YYYY-MM-DD");
     process.exit(-1);
 }
- 
+
 var param = process.argv[2];
 var sepline = "************************************************";
- 
+
 console.log(sepline);
 console.log('Scan for feeds on or after: ' + param);
 console.log(sepline + "\n");
@@ -26,38 +27,48 @@ http.get({
   let rawData = '';
   res.on('data', (chunk) => rawData += chunk);
   res.on('end', () => {
-    var cq = new CommandQueue();
+    let cq = new CommandQueue();
+    let dayindex = 0;
+    let currentPubDateString = "";
     let parsedData =  JSON.parse(parser.toJson(rawData)).rss.channel.item;
-    let podcastarr = parsedData.map(function(s) {
-      return {
-        filename: "videos/The Revolutionary Road Radio Show - " + dateFormat(new Date(s.pubDate),"yyyy-mm-dd") + ".webm",
-        url: s.enclosure.url,
-        pubDate: new Date(s.pubDate)
-      };
+    parsedData.filter(function(s) {
+      return  new Date(param) <= new Date(s.pubDate);
+    }).map(function(s) {
+      s.pubDate = new Date(s.pubDate);
+      s.url = s.enclosure.url;
+      if(currentPubDateString != dateFormat(s.pubDate,"yyyy-mm-dd")) {
+        s.filename = "videos/The Revolutionary Road Radio Show - " + dateFormat(new Date(s.pubDate),"yyyy-mm-dd") + ".webm";
+        dayindex = 0;
+        currentPubDateString = dateFormat(s.pubDate,"yyyy-mm-dd");
+      } else {
+        dayindex++;
+        s.filename = "videos/The Revolutionary Road Radio Show " + (dayindex+1) + " - " + dateFormat(new Date(s.pubDate),"yyyy-mm-dd") + ".webm";
+      }
+      return s;
+    }).filter(function(s) {
+      return !fs.existsSync(s.filename);
     }).map(function(s) {
       return {
-        filename: s.filename,
+	filename: s.filename,
         url: s.url,
         pubDate: s.pubDate,
         ffmpeg: "ffmpeg -loop 1 -i '" + "videos/logo.jpg" + "' -i  '" + s.url + "' -shortest -c:v libvpx-vp9 -c:a libopus -threads 2 -tile-columns 1 -speed 2 -auto-alt-ref 1 -lag-in-frames 25 '" + s.filename + "'"
       };
-    }).filter(function(s) {
-      return !fs.existsSync(s.filename) && new Date(param) <= s.pubDate;
     }).forEach(function(s) {
       console.log(sepline);
       console.log("Adding Command: \n" + s.ffmpeg);
       console.log(sepline + "\n");
       cq = cq.sync(s.ffmpeg);
     });
+    // Process the command queue
     cq
     .run()
-    .then(    
+    .then(
       function() {
-        console.log('success');
+        console.log('Video processing complete!');
       },
       function() {
         console.log('failure');
-            
         // Close any remaining commands. 
         queue.close();
       }
@@ -65,8 +76,5 @@ http.get({
   }).on('error', (e) => {
     console.log(e.message);
   });
+
 });
-
-
-
-
